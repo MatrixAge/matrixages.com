69594fc4fb47d64500da13da086333791d619737
MatrixAge <xiewendao123@foxmail.com>
2021-01-13 17:29:48 +0800

    [add] 优化介绍文案

--- components/Law/index.tsx
+++ components/Law/index.tsx
@@ -1,11 +1,21 @@
+import Image from 'next/image'
 import NavLink from '@/components/NavLink'
 import styles from './index.less'
 
 const Index = ({ className }: { className?: string }) => {
 	return (
-		<div className={`${styles._local} ${className}`}>
+		<div className={`${styles._local} ${className} flex`}>
+			<div className='beian gongan flex align_center mr_20'>
+				<Image
+					width={20}
+					height={20}
+					src='/images/icon_beian.png'
+					alt='icon_beian'
+				/>
+				<span className='ml_4'>浙公网安备 33011002015372号</span>
+			</div>
 			<NavLink
-				className='icp'
+				className='beian icp'
 				to='https://beian.miit.gov.cn'
 				target='_blank'
 				rel='noopener noreferrer'
